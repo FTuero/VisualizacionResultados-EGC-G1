ed6edea601d31d27e3f4bb096542ae5a812a9492
Manu <manuamores1212@gmail.com>
2017-12-19 20:49:48 +0100

    Porcentaje de la participación del censo en una encuesta

--- vr/controllers/db_controller.py
+++ vr/controllers/db_controller.py
@@ -1,6 +1,8 @@
 from ..model import *
 from .. import app
 from ..logic.poll_service import find_poll,votes_per_day
+from ..logic.census_service import census_count
+from flask import render_template
 from flask import jsonify
 
 @app.route('/visres/data/day/<poll_id>')
@@ -11,6 +13,15 @@ def votes_per_day_data(poll_id):
         len=poll.votos_actuales,
         labels=vpd[0],
         data=vpd[1]
+    )
+    return res
+
+@app.route('/visres/data/census/<poll_id>')
+def participation(poll_id):
+    poll = find_poll(poll_id)
+    participantsN = census_count(poll.census.id);
+    res= jsonify(
+        participation=participantsN/poll.votos_actuales
     )
     return res
 
---
+++ vr/logic/census_service.py
@@ -0,0 +1,8 @@
+from ..model import Census,session
+
+def census_count(census_id):
+    res = 0
+    census =session.query(Census).get(census_id)
+    for us in census.user_account_per_census:
+        res += 1
+    return res
