8d25b63f7e4afcaed5e7cb685a36fb10d01a02dd
Manu <manuamores1212@gmail.com>
2017-12-20 12:21:42 +0100

    Corrección error en la Query de la peticion de los votos por dia

--- vr/logic/poll_service.py
+++ vr/logic/poll_service.py
@@ -1,4 +1,6 @@
 from ..model import Poll, session
+import datetime
+from datetime import timedelta, date
 
 
 def find_poll(id):
@@ -8,16 +10,25 @@ def find_poll(id):
 def votes_per_day(poll_id):
     res = [[],[]]
     poll= session.query(Poll).get(poll_id)
+
+    def daterange(start_date, end_date):
+        for n in range(int((end_date - start_date).days)):
+            yield start_date + timedelta(n)
+
+    start_date = poll.startDate
+    end_date = poll.endDate
+    for single_date in daterange(start_date, end_date):
+        res[0].extend(single_date.strftime("%Y-%m-%d"))
+        res[1].extend(1)
+
+
     for q in poll.question_collection:
         for op in q.question_option_collection:
             for opv in op.option_per_vote_collection:
                 for vo in opv.vote_collection:
                     if(res[0].__contains__(vo.voteDate)):
                         i=res[0].index(vo.voteDate)
-                        res[1]=res[1].__getitem__(i)+1
-                    else:
-                        res[0].extend(vo.voteDate)
-                        res[1].extend(1)
+                        res[1].__setitem__(i,res[1].__getitem__(i))
     return res
 
 
