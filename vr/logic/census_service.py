54004e0872e2cb3637c74b8adbdeb06ece38c622
Manu <manuamores1212@gmail.com>
2017-12-30 16:42:04 +0100

    Contemplar la opcion de que no existan participantes admitidos en una encuesta en el calculo de porcentaje de participacion de una encuesta

--- vr/logic/census_service.py
+++ vr/logic/census_service.py
@@ -3,5 +3,7 @@ from ..model import Poll, session
 
 def census_count(poll_id):
     poll = session.query(Poll).get(poll_id)
+    if poll.participantes_admitidos==0:
+        return 0
     res=poll.votos_actuales/poll.participantes_admitidos
     return res
