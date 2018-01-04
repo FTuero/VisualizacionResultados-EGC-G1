7513b505fdc9c4453f7d5787f9db84542dd6452d
Maite <maitejr_95@hotmail.com>
2018-01-04 14:40:27 +0100

    Modificados los datos mostrados en la página principal de una encuesta (ahora se muestra el link al resultado de sus preguntas, el número de votos recibidos y el censo

--- vr/config.py
+++ vr/config.py
@@ -1,7 +1,7 @@
 import os
 
-CONFIG_BD_USER = os.environ['MARIADB_USER']
-CONFIG_BD_PASS = os.environ['MARIADB_PASSWORD']
-CONFIG_BD_DIR = os.environ['MARIADB_HOST']
+CONFIG_BD_USER = "root"#os.environ['MARIADB_USER']
+CONFIG_BD_PASS = "1234"#os.environ['MARIADB_PASSWORD']
+CONFIG_BD_DIR = "127.0.0.1"#os.environ['MARIADB_HOST']
 CONFIG_BD_NAME = "votaciones_splc"
-CONFIG_BD_PORT = os.environ['MARIADB_PORT']
+CONFIG_BD_PORT = "3306"#os.environ['MARIADB_PORT']
