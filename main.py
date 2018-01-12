f078895b8507f16a63b7f8e079470c5b63e0e8e0
Maite <maitejr_95@hotmail.com>
2018-01-12 20:59:35 +0100

    Añadido el decorador encargado de comprobar que el usuario está identificado en el sistema

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from vr import app
 from vr.model import base
-from vr.controllers import index_controller,db_controller
+from vr.controllers import index_controller,db_controller, login_required
 from flask import request, send_from_directory
 
 @app.route('/visres/static/<path:path>')
