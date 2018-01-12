747cd39bb9f3f6d16d3e53028e6e8488b945bc55
Maite <maitejr_95@hotmail.com>
2018-01-12 20:26:39 +0100

    Añadida la comprobación de identificación al método home de index_controller.py

--- vr/controllers/index_controller.py
+++ vr/controllers/index_controller.py
@@ -3,11 +3,24 @@ from .. import app
 from ..logic.poll_service import find_poll
 from ..logic.question_service import question_option_result
 from flask import render_template
+from flask import request
+from flask import redirect
+import requests
+from pprint import pprint
+import json
 
 @app.route('/visres/<poll_id>')
 def home(poll_id):
-    poll = find_poll(poll_id)
-    def count_question(id):
-        return question_option_result(id)
+    session_id = request.cookies.get("session_id")
 
-    return render_template('result.html', poll=poll, count_question=count_question)
+    if not session_id:
+        return redirect("https://g1login.egc.duckdns.org/login")
+
+    else:
+        r = requests.get('https://g1login.egc.duckdns.org/cookies/'+str(session_id))
+        r.json()
+        d = json.loads(r.text.decode('ascii', 'ignore'))
+        if d[codigo] == 0:
+            return redirect("https://g1login.egc.duckdns.org/login")
+        else:
+            return render_template('result.html', poll=poll, count_question=count_question)
